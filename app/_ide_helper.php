d0e550a5a97d5d761ae39e8dd13fcadbaa0e683e
haonx <qtvhao@gmail.com>
2018-07-20 01:08:43 +0700

    update ide helper

--- app/_ide_helper.php
+++ app/_ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.4.36 on 2018-07-19 16:31:22.
+ * Generated for Laravel 5.4.36 on 2018-07-19 18:07:25.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
