9e4482593e7888357df8d7b60fa0c34eefeba210
haonx <qtvhao@gmail.com>
2018-07-20 01:13:29 +0700

    add user-define Model

---
+++ app/app/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 7/19/2018
+ * Time: 11:32 PM
+ */
+
+namespace App;
+
+/** @mixin \Eloquent */
+class Model extends \Illuminate\Database\Eloquent\Model {
+
+}
