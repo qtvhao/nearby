69ea69f1dbabdc7f0c755a7cb96f5012ee651f3f
haonx <qtvhao@gmail.com>
2018-08-11 15:10:58 +0700

    use RecService

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 var getRandomIndex = function (myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 };
-var RecController = function ($http, $scope) {
+var RecController = function ($http, $scope, RecService) {
     var scope = $scope;
-    $http
-        .get('https://qtvhao.github.io/nearby/api/v2/recs/core.json')
-        .then(function (resp) {
+        RecService.all().then(function (resp) {
             scope.recs = resp.data;
             scope.rec = getRandomIndex(scope.recs);
         })
 };
 angular
     .module('myApp', [])
+    .service('RecService', function ($http) {
+        return {
+            all: function () {
+                return $http.get('https://qtvhao.github.io/nearby/api/v2/recs/core.json')
+            }
+        };
+    })
     .controller('RecController', RecController);
