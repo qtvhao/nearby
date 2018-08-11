41fdd34813f3131d70a1b16b3fcfe5a90fe61b1e
haonx <qtvhao@gmail.com>
2018-08-11 15:00:51 +0700

    rename Controller

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var getRandomIndex = function (myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 };
-angular.module('myApp', []).controller('myController', function ($http, $scope) {
+angular.module('myApp', []).controller('RecController', function ($http, $scope) {
     $http.get('https://qtvhao.github.io/nearby/api/v2/recs/core.json').then(function (resp) {
         var scope = $scope;
         scope.recs = resp.data;
