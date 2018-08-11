b9d7016986faa2f1c0222913cd27f2dea6095076
haonx <qtvhao@gmail.com>
2018-08-11 12:55:36 +0700

    implements Recs.index

--- app/resources/views/recs/index.blade.php
+++ app/resources/views/recs/index.blade.php
@@ -4,16 +4,21 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
                     <div class="panel-body">
                         @if (session('status'))
                             <div class="alert alert-success">
                                 {{ session('status') }}
                             </div>
                         @endif
-
-                        {{$rec}}
+                        {{$rec->name}}
+                        {{$rec->age}}
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <img src="{{$rec->image_url}}" alt="">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
