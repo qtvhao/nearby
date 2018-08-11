f51fee72d13c0e09d949841afac9de91c4f435c5
haonx <qtvhao@gmail.com>
2018-08-11 14:59:35 +0700

    use external app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var getRandomIndex = function (myArray) {
+    return myArray[Math.floor(Math.random() * myArray.length)];
+};
+angular.module('myApp', []).controller('myController', function ($http, $scope) {
+    $http.get('https://qtvhao.github.io/nearby/api/v2/recs/core.json').then(function (resp) {
+        var scope = $scope;
+        scope.recs = resp.data;
+        scope.rec = getRandomIndex(scope.recs);
+    })
+});
