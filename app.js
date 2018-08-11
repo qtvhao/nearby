e34052e6ca40cf234c6df99b9b341dcf7c9521d3
haonx <qtvhao@gmail.com>
2018-08-11 15:07:30 +0700

    refactoring

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var getRandomIndex = function (myArray) {
     return myArray[Math.floor(Math.random() * myArray.length)];
 };
-angular.module('myApp', []).controller('RecController', function ($http, $scope) {
-    $http.get('https://qtvhao.github.io/nearby/api/v2/recs/core.json').then(function (resp) {
-        var scope = $scope;
-        scope.recs = resp.data;
-        scope.rec = getRandomIndex(scope.recs);
-    })
-});
+var RecController = function ($http, $scope) {
+    var scope = $scope;
+    $http
+        .get('https://qtvhao.github.io/nearby/api/v2/recs/core.json')
+        .then(function (resp) {
+            scope.recs = resp.data;
+            scope.rec = getRandomIndex(scope.recs);
+        })
+};
+angular
+    .module('myApp', [])
+    .controller('RecController', RecController);
